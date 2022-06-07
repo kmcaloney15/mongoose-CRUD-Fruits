622409e4b5dbea825f55b1912fe444e3098f0802
kmcaloney15 <katymcaloney@gmail.com>
2022-06-07 14:02:43 -0400

    edit route working

--- server.js
+++ server.js
@@ -101,10 +101,14 @@ app.get("/fruits", async (req, res) => {
   res.render("fruits/index.liquid", { fruits });
 });
 
+
+
 //NEW ROUTE
 app.get("/fruits/new", (req, res) => {
   res.render("fruits/new");
 });
+
+
 
 // CREATE route
 app.post("/fruits", (req, res) => {
@@ -122,6 +126,68 @@ app.post("/fruits", (req, res) => {
       res.json({ error });
     });
 });
+
+
+// EDIT ROUTE
+app.get("/fruits/:id/edit", (req, res) => {
+  // get the id from params
+  const id = req.params.id;
+  // get the fruit from the database
+  Fruit.findById(id)
+    .then((fruit) => {
+      // render edit page and send fruit data
+      res.render("fruits/edit.liquid", { fruit });
+    })
+    // send error as json
+    .catch((error) => {
+      console.log(error);
+      res.json({ error });
+    });
+});
+
+// PUT ROUTE
+//update route
+app.put("/fruits/:id", (req, res) => {
+  // get the id from params
+  const id = req.params.id;
+  // check if the readyToEat property should be true or false
+  req.body.readyToEat = req.body.readyToEat === "on" ? true : false;
+  // update the fruit
+  Fruit.findByIdAndUpdate(id, req.body, { new: true })
+    .then((fruit) => {
+      // redirect to main page after updating
+      res.redirect("/fruits");
+    })
+    // send error as json
+    .catch((error) => {
+      console.log(error);
+      res.json({ error });
+    });
+});
+
+
+//DELETE ROUTE
+app.delete("/fruits/:id", (req, res) => {
+  // get the id from params
+  const id = req.params.id;
+  // delete the fruit
+  Fruit.findByIdAndRemove(id)
+    .then((fruit) => {
+      // redirect to main page after deleting
+      res.redirect("/fruits");
+    })
+    // send error as json
+    .catch((error) => {
+      console.log(error);
+      res.json({ error });
+    });
+});
+
+
+
+
+
+
 
 //SHOW ROUTE SHOULD ALWAYS BE NEAR TO BOTTOM TO AVOID MESS UP WITH EARLIER PAGES
 // show route
