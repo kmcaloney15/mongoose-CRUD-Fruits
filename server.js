44180dec3b38f4440e3ee897cb980642e1f4900b
kmcaloney15 <katymcaloney@gmail.com>
2022-06-07 11:43:52 -0400

    set up index route method

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const express = require("express"); // import express
 const morgan = require("morgan"); //import morgan
 const methodOverride = require("method-override");
 const mongoose = require("mongoose");
-const path = require("path")
+const path = require("path");
 
 /////////////////////////////////////////////
 // Database Connection
@@ -14,13 +14,13 @@ const path = require("path")
 // Setup inputs for our connect function
 const DATABASE_URL = process.env.DATABASE_URL;
 const CONFIG = {
-    useNewURLParser: true,
-    useUnifiedTopology: true,
-}
+  useNewURLParser: true,
+  useUnifiedTopology: true,
+};
 
 // const db = mongoose.connection //or could create this shortcut if called multiple times
 // Establish Connection
-mongoose.connect(DATABASE_URL, CONFIG)
+mongoose.connect(DATABASE_URL, CONFIG);
 
 // Events for when connection opens/disconnects/errors
 mongoose.connection
@@ -35,25 +35,25 @@ mongoose.connection
 //   const Schema = mongoose.Schema
 //   const model = mongoose.model
 //how you can create both at the same time instead of having to create the two seperate above ^
-  const { Schema, model} = mongoose;
+const { Schema, model } = mongoose;
 
 // make fruits schema
-const fruitsSchema = new Schema ({
-    name: String,
-    color: String,
-    readyToEat: Boolean,
+const fruitsSchema = new Schema({
+  name: String,
+  color: String,
+  readyToEat: Boolean,
 });
 
 // make fruit model
 const Fruit = model("Fruit", fruitsSchema);
 
-
 /////////////////////////////////////////////////
 // Create our Express Application Object Bind Liquid Templating Engine
 /////////////////////////////////////////////////
 //didn't include app at the top since it's only used here
-const app = require("liquid-express-views")(express(), {root: [path.resolve(__dirname, 'views/')]})
-
+const app = require("liquid-express-views")(express(), {
+  root: [path.resolve(__dirname, "views/")],
+});
 
 /////////////////////////////////////////////////////
 // Middleware
@@ -63,13 +63,47 @@ app.use(methodOverride("_method")); // override for put and delete requests from
 app.use(express.urlencoded({ extended: true })); // parse urlencoded request bodies
 app.use(express.static("public")); // serve files from public statically
 
-
 ////////////////////////////////////////////
 // Routes
 ////////////////////////////////////////////
 app.get("/", (req, res) => {
-    res.send(`your server is running... you better catch it.`)
+  res.send(`your server is running... you better catch it.`);
 });
+
+//any time you go to this link, it will delete all data and then add in only the data listed below
+//used to test that database is working without having to create new create page, add in data and then test
+app.get("/fruits/seed", (req, res) => {
+  // array of starter fruits
+  const startFruits = [
+    { name: "Orange", color: "orange", readyToEat: false },
+    { name: "Grape", color: "purple", readyToEat: false },
+    { name: "Banana", color: "orange", readyToEat: false },
+    { name: "Strawberry", color: "red", readyToEat: false },
+    { name: "Coconut", color: "brown", readyToEat: false },
+  ];
+
+  // Delete all fruits
+  Fruit.deleteMany({}).then((data) => {
+    // Seed Starter Fruits
+    Fruit.create(startFruits).then((data) => {
+      // send created fruits as response to confirm creation
+      res.json(data); //returning json data on route page
+    });
+  });
+});
+
+
+
+// The Async/Await Method //index route
+app.get("/fruits", async (req, res) => { //async looks for any kind of awaits - async knows it has to wait for await to finsh running before it will run it's function
+    const fruits = await Fruit.find({}) // Fruits.find({}) takes a long time to run
+    // await has it wait a second allowing Fruits.find({}) to run before it runs allowing the data to be retrived from the database
+    res.render("/fruits/index.liquid", {fruits})
+    
+})
+
+
+
 
 
 //////////////////////////////////////////////
@@ -77,7 +111,8 @@ app.get("/", (req, res) => {
 //////////////////////////////////////////////
 const PORT = process.env.PORT; // variable port that I'm pulling from the .env - this way you don't have to call it everytime you want to use it
 app.listen(PORT, () => {
-    console.log(`Now listening on port ${PORT}`)
-})
+  console.log(`Now listening on port ${PORT}`);
+});
+
 
 
